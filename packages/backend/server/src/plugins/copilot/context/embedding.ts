a0eee880f3102c7661a3961ccf37a774cb7f085b
DarkSky <darksky2048@gmail.com>
2025-04-24 17:50:41 +0800

    feat: rerank for matching

--- packages/backend/server/src/plugins/copilot/context/embedding.ts
+++ packages/backend/server/src/plugins/copilot/context/embedding.ts
@@ -2,11 +2,12 @@ import {
   createOpenAI,
   type OpenAIProvider as VercelOpenAIProvider,
 } from '@ai-sdk/openai';
-import { embedMany } from 'ai';
+import { embedMany, generateObject } from 'ai';
+import { chunk } from 'lodash-es';
 
-import { Embedding } from '../../../models';
+import { ChunkSimilarity, Embedding } from '../../../models';
 import { OpenAIConfig } from '../providers/openai';
-import { EmbeddingClient } from './types';
+import { EmbeddingClient, getReRankSchema, ReRankResult } from './types';
 
 export class OpenAIEmbeddingClient extends EmbeddingClient {
   readonly #instance: VercelOpenAIProvider;
@@ -34,6 +35,62 @@ export class OpenAIEmbeddingClient extends EmbeddingClient {
       embedding,
       content: input[index],
     }));
+  }
+
+  private async getEmbeddingRelevance<
+    Chunk extends ChunkSimilarity = ChunkSimilarity,
+  >(query: string, embeddings: Chunk[]): Promise<ReRankResult> {
+    const modelInstance = this.#instance('gpt-4.1-mini');
+    const results = embeddings.map(e => [
+      `
+  <result>
+    <targetId>${'docId' in e ? e.docId : 'fileId' in e ? e.fileId : ''}</targetId>
+    <chunk>${e.chunk}</chunk>
+    <content>${e.content}</content>
+  </result>`,
+    ]);
+    const {
+      object: { ranks },
+    } = await generateObject({
+      model: modelInstance,
+      prompt: `Generate a score array based on the search results list to measure the likelihood that the information contained in the search results is useful for the report on the following topic: ${query}\n\nHere are the search results:\n<results>${results}\n</results>`,
+      schema: getReRankSchema(1),
+      maxRetries: 3,
+    });
+    return ranks;
+  }
+
+  override async reRank<Chunk extends ChunkSimilarity = ChunkSimilarity>(
+    query: string,
+    embeddings: Chunk[],
+    topK: number
+  ): Promise<Chunk[]> {
+    const sortedEmbeddings = embeddings.toSorted(
+      (a, b) => (a.distance ?? Infinity) - (b.distance ?? Infinity)
+    );
+
+    const chunks = sortedEmbeddings.reduce(
+      (acc, e) => {
+        const key = `${'docId' in e ? e.docId : 'fileId' in e ? e.fileId : ''}:${e.chunk}`;
+        acc[key] = e;
+        return acc;
+      },
+      {} as Record<string, Chunk>
+    );
+
+    const ranks = await Promise.all(
+      chunk(sortedEmbeddings, topK).map(e =>
+        this.getEmbeddingRelevance(query, e)
+      )
+    );
+
+    const highConfidenceChunks = ranks
+      .flat()
+      .toSorted((a, b) => b.scores.score - a.scores.score)
+      .filter(r => r.scores.score > 5)
+      .map(r => chunks[`${r.scores.targetId}:${r.scores.chunk}`]);
+
+    return highConfidenceChunks.slice(0, topK);
   }
 }
 
