0a37d20528801f27565bd857d023018baaff6915
Jimmfly <yangjinfei001@gmail.com>
2025-04-25 12:06:45 +0800

    refactor(core): improve doc loading tracking with waitForDocLoaded and error handling

--- packages/frontend/core/src/blocksuite/block-suite-editor/blocksuite-editor.tsx
+++ packages/frontend/core/src/blocksuite/block-suite-editor/blocksuite-editor.tsx
@@ -9,6 +9,7 @@ import {
   fontStyleOptions,
 } from '@affine/core/modules/editor-setting';
 import { FeatureFlagService } from '@affine/core/modules/feature-flag';
+import { WorkspaceService } from '@affine/core/modules/workspace';
 import track from '@affine/track';
 import { appendParagraphCommand } from '@blocksuite/affine/blocks/paragraph';
 import type { DocTitle } from '@blocksuite/affine/fragments/doc-title';
@@ -291,6 +292,7 @@ export const BlockSuiteEditor = (props: EditorProps) => {
   const [isLoading, setIsLoading] = useState(true);
   const [longerLoading, setLongerLoading] = useState(false);
   const [loadStartTime] = useState(Date.now());
+  const workspaceService = useService(WorkspaceService);
 
   const editorSetting = useService(EditorSettingService).editorSetting;
   const settings = useLiveData(
@@ -317,24 +319,11 @@ export const BlockSuiteEditor = (props: EditorProps) => {
   useEffect(() => {
     if (props.page.root) {
       setIsLoading(false);
-      track.doc.$.$.loadDoc({
-        workspaceId: props.page.workspace.id,
-        docId: props.page.id,
-        // time cost in ms
-        time: Date.now() - loadStartTime,
-        success: true,
-      });
       return;
     }
 
     const disposable = props.page.slots.rootAdded.subscribe(() => {
       disposable.unsubscribe();
-      track.doc.$.$.loadDoc({
-        workspaceId: props.page.workspace.id,
-        docId: props.page.id,
-        time: Date.now() - loadStartTime,
-        success: true,
-      });
       setIsLoading(false);
       setLongerLoading(false);
     });
@@ -364,6 +353,27 @@ export const BlockSuiteEditor = (props: EditorProps) => {
       clearTimeout(reportErrorTimer);
     };
   }, [isLoading, loadStartTime, props.page]);
+
+  useEffect(() => {
+    workspaceService.workspace.engine.doc
+      .waitForDocLoaded(props.page.id)
+      .then(() => {
+        track.doc.$.$.loadDoc({
+          workspaceId: props.page.workspace.id,
+          docId: props.page.id,
+          time: Date.now() - loadStartTime,
+          success: true,
+        });
+      })
+      .catch(() => {
+        track.doc.$.$.loadDoc({
+          workspaceId: props.page.workspace.id,
+          docId: props.page.id,
+          time: Date.now() - loadStartTime,
+          success: false,
+        });
+      });
+  }, [loadStartTime, props.page, workspaceService]);
 
   return (
     <Slot style={{ '--affine-font-family': fontFamily } as CSSProperties}>
