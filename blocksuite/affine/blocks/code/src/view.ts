3471353c1aeea9a4570a7fed9e4064fc61cbc18a
flrande <flrande.ch@gmail.com>
2025-04-25 16:23:40 +0800

    fix: fix

--- blocksuite/affine/blocks/code/src/view.ts
+++ blocksuite/affine/blocks/code/src/view.ts
@@ -20,6 +20,7 @@ import { CodeKeymapExtension } from './code-keymap.js';
 import { AFFINE_CODE_TOOLBAR_WIDGET } from './code-toolbar/index.js';
 import { codeSlashMenuConfig } from './configs/slash-menu.js';
 import { effects } from './effects.js';
+import { CodeBlockHtmlPreview } from './preview/renderer/html/html-render.js';
 
 export const codeToolbarWidget = WidgetViewExtension(
   'affine:code',
@@ -44,6 +45,7 @@ export class CodeBlockViewExtension extends ViewExtensionProvider {
       codeToolbarWidget,
       SlashMenuConfigExtension('affine:code', codeSlashMenuConfig),
       CodeKeymapExtension,
+      CodeBlockHtmlPreview,
       ...getCodeClipboardExtensions(),
     ]);
     context.register([
